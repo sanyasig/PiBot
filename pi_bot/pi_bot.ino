de1149dfed50d457da1ee5dd23ce58252242814e
sanyasi_g <nani76.gsrao@gmail.com>
2014-11-23 18:29:52 +0000

    working serial

--- pi_bot/pi_bot.ino
+++ pi_bot/pi_bot.ino
@@ -1,135 +1,99 @@
-//#include <aJSON.h>
-#include <Servo.h>
-#include <AFMotor.h>
+char* replyOK = {
+  "OK\n"};
 
-// json paramerter
-char Orders[3];
-char current_mode = 't';
+#define MAX_COMMAND_LENGTH 255
+char resetByte = '!';
+char stopByte = '#';
 
-//dc motor controls
-AF_DCMotor motor1(1, MOTOR12_64KHZ);
-AF_DCMotor motor2(1, MOTOR12_64KHZ);
+char* commandLEDOn = {
+  "LEDON"};
+char* commandLEDOff = {
+  "LEDOFF"};
 
-// ping sensor pins 
-int tri_pin = 14;
-int echo_pin = 15;
+int incomingByte = 0;
+int charCount = 0;
+char command[MAX_COMMAND_LENGTH + 1]; // leave space for \0
+bool locked = false;
+int led = 13;
+int led1 = 8;
+int led2 = 9;
+int led3 = 10;
+int led4 = 11;
 
-// servo for ping sensor
-unsigned long last_print = 0;
-Servo servo;
-int servo_pin = 10;
-
-// modes
-#define Contorl_Mode 0
-#define Dir          1#define Time         2
-#define min_distance 20 // minimubum distance to be considered as a obstrical
-
-int right_distance = 0;
- int left_distance = 0;
-
-void setup(){
-  
- Serial.begin(9600);
- // inilize the json objects
- // a- autonomous, m-manual 
- Orders[Contorl_Mode] = 'a';
- // f- forward, r- right, l -left, b- backwords
- Orders[1] = 'f';
- Orders[2] = '1';
- 
- servo.attach(servo_pin);
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop(){
-  if(Serial.available() > 0){
-    Serial.println("revceivin info over serival");
-    Serial.readBytesUntil('/r',Orders,3);
-    // check the control mode 
-    if(Orders[Contorl_Mode] == 'a'){
-      if(current_mode != 'a'){
-        current_mode = 'a';
-        activate_autonomous_mode();
+void loop() {
+  if (Serial.available()) {
+    incomingByte = Serial.read();
+
+    if ((char)incomingByte == resetByte) {
+      locked = false;
+      charCount = 0;
+      return;
+    }
+
+    if (locked) {
+      Serial.println("@");
+      return;
+    }
+
+    if ((char)incomingByte != stopByte) {
+      if (charCount > MAX_COMMAND_LENGTH - 1) {
+        Serial.println("@");
+        locked = true;
+        return;
+      }
+      else {
+        command[charCount] = (char)incomingByte;
+        charCount++;
+      }
+    }
+    else {
+      command[charCount] = '\0';
+      charCount = 0;
+      displayLights(command);
+      if (strcmp(commandLEDOn, command) == 0) {
+        digitalWrite(led, HIGH);
+        Serial.println(replyOK);
+      }
+      else if (strcmp(commandLEDOff, command) == 0) {
+        digitalWrite(led, LOW);
+        Serial.println(replyOK);
       }
     }
   }
-    
-  if(Orders[Contorl_Mode] == 'm'){
-      current_mode = 'm';
-      
-      // I have the power,, no need of stupid ord
-  }
-    
 }
 
+void displayLights(String commad){
+//  if(command.length() < 2){
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
 
-
-//method to get distance from the nearest object
-int get_distance()
-{
-  int dis,duration;
-  digitalWrite(tri_pin,HIGH);
-  delay(150);
-  digitalWrite(tri_pin, LOW);
-  duration = pulseIn(echo_pin, HIGH);
-  int obj_dist = (duration/2)/29.1;
-  Serial.println("distance from nearest obstrical is" + obj_dist);
-  Serial.println(obj_dist);
-  delay(300);
-  return obj_dist;
-}
-
-//autonomous mode
-void activate_autonomous_mode(){
-  Serial.println("in automousmode");
-  servo.write(90);
-  if(get_distance() > min_distance){
-  //  go_forward();
-    delay(1000);
-   // all_stop();
-  }
-  else{
-    //lok right
-    servo.write(0);
-    delay(500);
-    left_distance = get_distance();
-    // look left
-    servo.write(180);
-    delay(500);
-    right_distance = get_distance();
-    //turn left
-    if(left_distance > right_distance){
-      turn_left();
+    if(command[0]== 'r' || command[1] == 'r'){
+      digitalWrite(led1, HIGH);
     }
-    // turn right
-    else{
-      turn_right();
+    if(command[0]== 'l' || command[1] == 'l'){
+      digitalWrite(led4, HIGH);
     }
-   
-  }
+    if(command[0]== 'f' || command[1] == 'f'){
+      digitalWrite(led2, HIGH);
+    }
+    if(command[0]== 'b' || command[1] == 'b'){
+      digitalWrite(led3, HIGH);
+    }
+
+
+ // }
+
+
 }
 
-// turn left
-void turn_left()
-{
-motor1.run(FORWARD);
-motor2.run(BACKWARD);
-delay(400);
-stop();
-}
-
-// turn right
-void turn_right()
-{
-motor1.run(BACKWARD);
-motor2.run(FORWARD);
-delay(500);
-stop();
-}
-
-//all stop
-void stop(){
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-}
-  
-  
